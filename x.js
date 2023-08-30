732cb84745b00f11b9c0f3074a81f2f82f061159
Nisar Ahmed <drnisar@hotmail.com>
2023-08-30 11:27:50 +0500

    genres basic routing created

---
+++ x.js
@@ -0,0 +1,13 @@
+// let courses = [
+//   { id: 1, name: "React" },
+//   { id: 2, name: "Angular" },
+//   { id: 3, name: "Vue" },
+// ];
+
+let courses = ["React", "Angular", "Vue"];
+
+let course = { id: 1, name: "React" };
+
+let index = courses.indexOf("Vue");
+
+console.log(index);
