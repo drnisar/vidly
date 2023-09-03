076b49457032d9aff9669da8509378ff0c5dea1d
Nisar Ahmed <drnisar@hotmail.com>
2023-09-03 17:43:59 +0500

    Movies added, though issues with errors reading

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
 const express = require("express");
 const app = express();
 
@@ -13,6 +14,7 @@ mongoose
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
 
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
