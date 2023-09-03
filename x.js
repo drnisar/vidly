286d6239d2d321ef834da2a724802410cca0a038
Nisar Ahmed <drnisar@hotmail.com>
2023-09-03 19:08:20 +0500

    Rental and rentals added

--- x.js
+++ x.js
@@ -1,14 +1,4 @@
 const Joi = require("joi");
-let util = require("util");
+const mongoose = require("mongoose");
 
-const validateCustomer = function (customer) {
-  const schema = Joi.object({
-    name: Joi.string().min(5).max(50).required(),
-  });
-
-  return schema.validate(customer);
-};
-
-const obj = validateCustomer({ name: "" });
-
-console.log(obj);
+console.log(mongoose.model());
